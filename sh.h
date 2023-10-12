ad0f9acc027fe10cfd6f4ffee121e2444239e567
Badr_Annabi <Badrannabi@gmail.com>
2023-10-12 09:40:40 -0700

    TEST

---
+++ sh.h
@@ -0,0 +1,13 @@
+#ifndef SH_H
+#define SH_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#endif
