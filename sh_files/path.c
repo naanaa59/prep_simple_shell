d6be08dce364a5cc29aab2015168fee75e2a2ea8
oumaima <oumaimanaanaapro@gmail.com>
2023-10-13 16:08:37 +0100

    path

--- sh_files/path.c
+++ sh_files/path.c
@@ -8,7 +8,7 @@
  * Return: 0 on success or -1 on failure
  */
 
-int ex_in_path(const char *filename)
+char *full_command(const char *filename)
 {
 	char *path_var;
 	char *path_dir;
@@ -16,28 +16,24 @@ int ex_in_path(const char *filename)
 	struct stat buffer;
 
 	if (filename == NULL)
-		return (-1);
+		return (NULL);
 	path_var = getenv("PATH");
 	if (path_var == NULL)
-		return (-1);
+		return (NULL);
 
 	path_dir = strtok(path_var, ":");
 	while (path_dir != NULL)
 	{
 		full_path = malloc(strlen(path_dir) + 1 + strlen(filename) + 1);
 		if (full_path == NULL)
-			return (-1);
+			return (NULL);
 		strcpy(full_path, path_dir);
 		if(full_path[strlen(path_dir) - 1] != '/')
 			strcat(full_path, "/");
 		strcat(full_path, filename);
 		if(stat(full_path, &buffer) != 0)
-		{
-			free(full_path);
-			return (-1);
-		}
-		free(full_path);
+			return (NULL);
 		path_dir = strtok(NULL, ":");
 	}
-	return (0);
+	return (full_path);
 }
